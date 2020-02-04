36626862b2fb2beedd524c8ada678a24b8d82c62
Nilsen Reyes <nreyes1202@yahoo.com>
2020-02-04 00:22:22 -0700

    renamed containers and added variables

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,25 @@
 $(document).ready(function () {
 
 
-    var topics [
-
-    ]
-
-
+  var topics = ['parks and rec', 'the good place', 'doctor who', 'the office', 'the walking dead', 'golden girls', 'schitts creek',
+    'breaking bad', 'westworld', 'saturday night live', 'unbreakable kimmy schmidt',
+  ],
 
 
-    
 
-var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=rseYwBLyvT0dvfCZ8DCjwzWA85NNlIDP";
 
-    
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-    
-    });
+
+  var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=rseYwBLyvT0dvfCZ8DCjwzWA85NNlIDP";
+
+
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+
+  });
 
 
 

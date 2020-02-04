88a4bead3d7e2f87ed0f5f19dd8e7ba8cf70bd22
Nilsen Reyes <nreyes1202@yahoo.com>
2020-02-03 23:21:35 -0700

    updated html and added buttons

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+
+    var topics [
+
+    ]
+
+
+
+
+    
+
+var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=rseYwBLyvT0dvfCZ8DCjwzWA85NNlIDP";
+
+    
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+    
+    });
+
+
+
+
+
+
+
+})

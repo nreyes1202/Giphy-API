09b9a44cf2736897c0af8a53fc00a7bb7acee841
Nilsen Reyes <nreyes1202@yahoo.com>
2020-02-04 23:38:42 -0700

    began function to render gif buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,24 @@
 $(document).ready(function () {
 
-
   var topics = ['parks and rec', 'the good place', 'doctor who', 'the office', 'the walking dead', 'golden girls', 'schitts creek',
     'breaking bad', 'westworld', 'saturday night live', 'unbreakable kimmy schmidt',
   ],
 
+  function renderButtons() {
+
+    $("#buttons-view").empty();
+
+    for (var i = 0; i < topics.length; i++) {
+      
+      var newGif = $("<button>").text(topics[i]);
+      newGif = attr("data-name", topics[i]);
+      newGif.addClass("gif");
+      $("#buttons-view").append(topics);
+    }
 
 
 
-
-
+  }
   var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=rseYwBLyvT0dvfCZ8DCjwzWA85NNlIDP";
 
 
@@ -22,5 +31,6 @@ $(document).ready(function () {
   });
 
 
-})
 
+
+}
